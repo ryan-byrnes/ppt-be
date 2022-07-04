3a786e2f2d065e2be01b7faa60c5075c12434253
Ryan Byrnes <ryn_byrnes@yahoo.com>
2022-07-04 11:22:41 -0700

    working on organization and creating exercise/pr models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ryan-byrnes/ppt-be
 
 go 1.18
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
---
+++ models/models.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Exercise struct {
+	Id           int    `json:"id"`
+	ExerciseName string `json:"exercise_name"`
+}
+
+type PersonalRecord struct {
+	Id         int       `json:"id"`
+	ExerciseId int       `json:"exercise_id"`
+	Reps       int       `json:"reps"`
+	Weight     int       `json:"weight"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
